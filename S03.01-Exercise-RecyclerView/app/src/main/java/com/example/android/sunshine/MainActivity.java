4af1995b3450d5a85b09f95c40745f7bcd2e96d3
Vicuko <victorc.fernandez@gmail.com>
2018-07-13 18:29:50 -0400

    Mark tasks as done

--- S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -242,7 +242,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         if (id == R.id.action_refresh) {
-            // TODO (46) Instead of setting the text to "", set the adapter to null before refreshing
+            // TODO (46) Instead of setting the text to "", set the adapter to null before refreshing - Done
             mForecastAdapter.setWeatherData(null);
             loadWeatherData();
             return true;
