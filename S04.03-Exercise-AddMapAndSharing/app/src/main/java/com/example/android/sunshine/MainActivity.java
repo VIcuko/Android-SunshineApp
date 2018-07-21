2172d87c08a43e6d6ffdff5fc12b944175b661d7
Vicuko <victorc.fernandez@gmail.com>
2018-07-21 14:43:10 -0400

    Create intent from menu element to open map app in device and create new menu for DetailActivity

--- S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -17,13 +17,11 @@ package com.example.android.sunshine;
 
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -221,8 +219,13 @@ public class MainActivity extends AppCompatActivity implements ForecastAdapterOn
             return true;
         }
 
-        // TODO (2) Launch the map when the map menu item is clicked
-
+        // TODO (2) Launch the map when the map menu item is clicked - Done
+        if (id == R.id.action_map) {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            }
+        }
         return super.onOptionsItemSelected(item);
     }
 }
