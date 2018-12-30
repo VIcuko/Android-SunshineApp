125971705fb24e704ebc4cfbc16bf452636ba50e
Vicuko <victorc.fernandez@gmail.com>
2018-12-30 12:53:07 +0100

    Create Settings Activity to manage preferences

---
+++ S06.01-Exercise-LaunchSettingsActivity/app/src/main/java/com/example/android/sunshine/utilities/SettingsActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.sunshine.utilities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.sunshine.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings2);
+
+    }
+}
