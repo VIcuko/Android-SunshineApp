1b917e77e657fe747b028f4a8e18cc1b8ea4533e
Vicuko <victorc.fernandez@gmail.com>
2018-12-30 13:41:11 +0100

    Overwrite home button to be a back button in settings screen

--- S06.01-Exercise-LaunchSettingsActivity/app/src/main/java/com/example/android/sunshine/utilities/SettingsActivity.java
+++ S06.01-Exercise-LaunchSettingsActivity/app/src/main/java/com/example/android/sunshine/utilities/SettingsActivity.java
@@ -2,6 +2,7 @@ package com.example.android.sunshine.utilities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.example.android.sunshine.R;
 
@@ -11,6 +12,17 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    // TODO (2) Set setDisplayHomeAsUpEnabled to true on the support ActionBar - Done
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.home){
+            onBackPressed();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
