8cd7402dc640aab7b99313e47fe6589b916290ab
Vicuko <victorc.fernandez@gmail.com>
2019-01-07 19:42:55 +0100

    Implement delete method to delete all the content in the table and notify provider of change

--- S09.03-Exercise-ContentProviderDelete/app/src/main/java/com/example/android/sunshine/data/WeatherProvider.java
+++ S09.03-Exercise-ContentProviderDelete/app/src/main/java/com/example/android/sunshine/data/WeatherProvider.java
@@ -293,7 +293,7 @@ public class WeatherProvider extends ContentProvider {
         return cursor;
     }
 
-//  TODO (1) Implement the delete method of the ContentProvider
+//  TODO (1) Implement the delete method of the ContentProvider - Done
     /**
      * Deletes data at a given URI with optional arguments for more fine tuned deletions.
      *
@@ -304,11 +304,25 @@ public class WeatherProvider extends ContentProvider {
      */
     @Override
     public int delete(@NonNull Uri uri, String selection, String[] selectionArgs) {
-        throw new RuntimeException("Student, you need to implement the delete method!");
+//      TODO (2) Only implement the functionality, given the proper URI, to delete ALL rows in the weather table - Done
+        int deletedRows;
+        if (null == selection) selection = "1";
+        int match = sUriMatcher.match(uri);
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
 
-//          TODO (2) Only implement the functionality, given the proper URI, to delete ALL rows in the weather table
-
-//      TODO (3) Return the number of rows deleted
+        switch (match){
+            case CODE_WEATHER:
+                deletedRows = db.delete(WeatherContract.WeatherEntry.TABLE_NAME,
+                        selection, selectionArgs);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unkown uri: " + uri);
+        }
+        if (deletedRows != 0) {
+//      TODO (3) Return the number of rows deleted - Done
+            getContext().getContentResolver().notifyChange(uri,null);
+        }
+        return deletedRows;
     }
 
     /**
